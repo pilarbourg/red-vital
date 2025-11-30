d66555a97f4d8f173ebca2f39a852a43fa1d1259
Olivia Gallego Toscano <o.gallego1@usp.ceu.es>
2025-11-30 17:15:33 +0100

    usuario id true

--- backend/db/models/hospital.js
+++ backend/db/models/hospital.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
     },
     usuario_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
     },
     nombre: {
         type: DataTypes.STRING,
